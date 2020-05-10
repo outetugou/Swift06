e9f1105213701b73db16a3690fe1f8b9fc89a9ca
outetugou <outetugou@gmail.com>
2020-05-10 11:47:53 +0900

    first commit of swift06

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+  platform :ios, '13.2'
+
+target 'Swift06' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Swift06
+
+  # Pods for Swift06
+  pod 'RxSwift', '~> 5'
+
+end
---
+++ Swift06/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  Swift06
+//
+//  Created by 王铁刚 on R 2/04/24.
+//  Copyright © Reiwa 2 王铁刚. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let prices = [100, 250, 560, 980]
+        let taxRate = 1.08
+
+        Observable
+            .from(prices)
+            .map({ price in
+                Int(Double(price) * taxRate)
+            })
+            .subscribe({ event in
+                print(event)
+            })
+            .dispose()
+        
+        test()
+    }
+
+    func test() {
+        let disposeBag = DisposeBag()
+        
+        // Subject作成
+        let subject = PublishSubject<String>()
+        subject.onNext("🍎")
+        
+        // Observer1購読
+        subject
+            .subscribe(onNext: { element in
+                print("Observer: 1 - Event: \(element)")
+            }, onCompleted: {
+                print("Observer: 1 - Event: completed")
+            }, onDisposed: {
+                print("Observer: 1 - Event: disposed")
+            })
+            .disposed(by: disposeBag)
+        
+        subject.onNext("🍣")
+        
+        // Observer2購読
+        subject
+            .map({ element in
+                "\(element) is nice!"
+            })
+            .subscribe(onNext: { element in
+                print("Observer: 2 - Event: \(element)")
+            }, onCompleted: {
+                print("Observer: 2 - Event: completed")
+            }, onDisposed: {
+                print("Observer: 2 - Event: disposed")
+            })
+            .disposed(by: disposeBag)
+
+        subject.onNext("🍔")
+        
+        // Observer3購読
+        subject
+            .subscribe(onNext: { element in
+                print("Observer: 3 - Event: \(element)")
+            }, onCompleted: {
+                print("Observer: 3 - Event: completed")
+            }, onDisposed: {
+                print("Observer: 3 - Event: disposed")
+            })
+            .disposed(by: disposeBag)
+        
+        subject.onNext("🍜")
+        
+        subject.onCompleted()
+    }
+}
+
